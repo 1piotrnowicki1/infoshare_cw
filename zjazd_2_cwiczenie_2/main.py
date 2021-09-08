37be16d037f416eae044803e2d5e63bb64cfcbe9
Piotr Nowicki <piotr.nowicki92@gmail.com>
2021-09-08 18:39:07 +0200

    init

---
+++ zjazd_2_cwiczenie_2/main.py
@@ -0,0 +1,11 @@
+"""
+1. stworzyć zmienną baza1 z 5 osobami gdzie kazda osoba to tupla z imieniem i nazwiskiem. Baza jest listą tych tupli
+2. baza2 będzie słownikiem, gdzie kluczem będą tuple z imieniem i nazwiskiem a wartością nr telefonu
+3. stworzyć funkcję która za pomocą for loop będzie w baza1 szukać osoby imieniem Andrzej - wtedy kończymy program.
+   Jeżeli imie tej osogy to Marek to wyprintujemy "Cześć Marek"
+4. stworzyć funkcję która za pomocą pętli while będzie szukać Karoliny Nowak - jeśli ją znajdziemy to chcemy wyświetlić jej nr tel
+5. dodać typowanie do funkcji
+
+6. funkcje uruchamiamy w bloku if __ name __ == '__ main __':
+
+"""
