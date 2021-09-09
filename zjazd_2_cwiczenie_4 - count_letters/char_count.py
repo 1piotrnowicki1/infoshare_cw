5b897677475381a07f50befc65bce079f68de555
Piotr Nowicki <piotr.nowicki92@gmail.com>
2021-09-09 12:41:57 +0200

    init

---
+++ zjazd_2_cwiczenie_4 - count_letters/char_count.py
@@ -0,0 +1,28 @@
+"""
+Napisz program który policzy liczbę liter w danym stringu.
+
+my_string = "The quick brown fox jumps over the lazy dog"
+
+"""
+
+# example_dict = {'a': 1, 'b': 2, 'c': 3}
+# example_dict['a']
+
+# example_dict.get('a')
+
+# example_dict['c'] = 1   - Adding key with value into dict
+
+
+my_string = "The quick brown fox jumps over the lazy dog"
+my_dict = {}
+
+my_string = my_string.lower()
+for char in my_string:
+    print(char)
+    if char in my_dict:
+        my_dict[char] += 1
+    else:
+        my_dict[char] = 1
+    print(my_dict)
+    print(10* '*')
+print(my_dict)
